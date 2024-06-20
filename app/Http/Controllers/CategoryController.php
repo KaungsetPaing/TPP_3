c5b565b9e95b4bc2f0c6cca1898bf03e5e725b50
Kaung set Paing <kaungsetpaing@Kaungs-MacBook-Pro.local>
2024-06-20 13:07:56 +0630

    login update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,10 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index(){
 
 
