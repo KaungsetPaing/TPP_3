6e241e850e769a2128c6d964ae25c5dbba542b37
Kaung set Paing <kaungsetpaing@Kaungs-MacBook-Pro.local>
2024-06-20 13:33:15 +0630

    db seed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
                                         Product
                                     </a>
                                     <a
-                                        href="{{route('categoryStore')}}"
+                                        href="{{route('categoryIndex')}}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Category
